9a9480657c5aba60532589c939c550d4391c80f0
stefan <stefan@stefan-desktop>
2015-03-21 17:46:41 +0100

    index: Prototypischer Aufruf von ImportPrices.

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+use Scmoeller\Finance\ImportPrices;
+use Scmoeller\Finance\Security;
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,6 +10,14 @@
  */
 require_once 'src/Scmoeller/Finance/ImportPrices.php';
 
-$import = new \Scmoeller\Finance\ImportPrices();
+$security = new Security("DE0008232125", "Deutsche Lufthansa", "LHA");
+
+$market = new Market(1, "XETRA", "DE");
+
+$startDate = new DateTime('2015-01-02');
+
+$endDate = new DateTime('2015-01-30');
+
+$import = new ImportPrices($security, $market, $startDate, $endDate);
 
 $import->execute();
